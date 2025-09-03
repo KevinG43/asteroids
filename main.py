871fde6451068e23c8c46d2fb66cb876b4864438
KevinG43 <kevingonzalez040302@gmail.com>
2025-09-03 09:48:48 -0600

    limited fps to 60

--- main.py
+++ main.py
@@ -6,10 +6,13 @@ from constants import *
 
 def main():
     pygame.init()
+    fps = pygame.time.Clock()
+    dt = 0
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) 
+
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
         for event in pygame.event.get():
@@ -17,6 +20,7 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = fps.tick(60)/1000
 
 if __name__ == "__main__":
     main()
